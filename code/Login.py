906ca19ddc643da2fb788295325233e9ab0f07cd
aaalana <41637549+aaalana@users.noreply.github.com>
2019-03-26 12:53:22 +1100

    Added empty files for the web app

---
+++ code/Login.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def test_page():
+    return "Now we just need the whole finished project (Geographically) to put on the server..."
