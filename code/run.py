b21d5ed6974cd6a075e6f75dda8e85f726dae621
growell3 <johndoe@example.com>
2019-03-27 03:52:08 +0000

    Comments to 'run.py'

--- code/run.py
+++ code/run.py
@@ -1,10 +1,13 @@
+### Install dependancies/libraries
 from flask import Flask
 
+### Set 'flask' environment settings
 app = Flask(__name__)
 
 @app.route('/')
 def test_page():
     return "Now we just need the whole finished project (Geographically) to put on the server..."
 
+### Start the server, (called through 'serverStart' script)
 if __name__ == '__main__':
     app.run()
