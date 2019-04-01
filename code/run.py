33dfb7f8a35d78a1bf8fb41eb81350bb527d047f
growell3 <johndoe@example.com>
2019-04-01 07:06:11 +0000

    Minor modifications

--- code/run.py
+++ code/run.py
@@ -1,14 +1,14 @@
 ### Import dependancies/libraries
-from flask import Flask
+from flask import Flask, request, abort, jsonify, url_for
 from flask_restful import Api, Resource
-from pymongo import MongoClient
+import pymongo
 
 ### Import resources
 from resources.users.py import Users
 
 ### Set up/connect to the Mongo Database
 client = MongoClient("mongodb://localhost:27017/")
-db = client.geographicallyDB
+db = client.geographically_DB
 users = db.users
 
 ### Set 'flask' environment settings
@@ -18,6 +18,24 @@ api = Api(app)
 @app.route('/')
 def test_page():
     return "Now we just need the whole finished project (Geographically) to put on the server..."
+
+
+
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    myquery = { "username": username }
+    mydoc = mycol.find(myquery)
+    if mydoc is not None:
+        abort(400) # existing user
+    user = User(username = username)
+#    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
 
 ### Add the Flask_RESTful resources here
 api.add_resource(Users, '\Users')
