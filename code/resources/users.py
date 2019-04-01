e6bc16e5d269fda903fc3ea06823e73ddc0b0e2d
growell3 <johndoe@example.com>
2019-04-01 06:23:54 +0000

    Tested some 'flask_restful' and pymongo

---
+++ code/resources/users.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+
+class Users(Resource):
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass 
--- code/run.py
+++ code/run.py
@@ -1,12 +1,26 @@
-### Install dependancies/libraries
+### Import dependancies/libraries
 from flask import Flask
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
+### Import resources
+from resources.users.py import Users
+
+### Set up/connect to the Mongo Database
+client = MongoClient("mongodb://localhost:27017/")
+db = client.geographicallyDB
+users = db.users
 
 ### Set 'flask' environment settings
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def test_page():
     return "Now we just need the whole finished project (Geographically) to put on the server..."
+
+### Add the Flask_RESTful resources here
+api.add_resource(Users, '\Users')
 
 ### Start the server, (called through 'serverStart' script)
 if __name__ == '__main__':
