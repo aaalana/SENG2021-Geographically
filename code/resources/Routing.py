27176fdcb7fd7877e098ef9095694c448a072a77
Elizabeth <z5209166@student.unsw.edu.au>
2019-04-08 14:51:44 +1000

    Converting return values to json

--- code/resources/Routing.py
+++ code/resources/Routing.py
@@ -1,18 +1,23 @@
+from flask_restful import Resource, Api
+from flask import Flask, request, render_template, jsonify
 import json, requests
-import googlemaps
 
 APIKEY = 'AIzaSyAFkE9C-jZGN-bocvETUHsJFm-F0cEhrVE'
 
-def findRoute(start, finish):
-    #start = "Bridgewater, Sa, Australia"
-    #finish = "Stirling, SA, Australia"
+class Route(Resource):
+    def findRouteInfo(start, end):
+        #start = "Bridgewater, Sa, Australia"
+        #finish = "Stirling, SA, Australia"
 
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin={start}&destination={finish}&key={APIKEY}".format(start = start, finish = finish, APIKEY=APIKEY)
-    response = requests.get(url)
-    res = json.loads(response.text)
+        url = "https://maps.googleapis.com/maps/api/directions/json?origin={start}&destination={end}&key={APIKEY}".format(start = start, end = end, APIKEY=APIKEY)
+        response = requests.get(url)
+        res = json.loads(response.text)
 
-    print(res["routes"][0]["legs"][0]["start_address"] + " to " + res["routes"][0]["legs"][0]["end_address"] +" Distance: " + res["routes"][0]["legs"][0]["distance"]["text"] + " Duration" +  res["routes"][0]["legs"][0]["duration"]["text"])
-    #print(res["routes"]["legs"]["start_address"])
-        #print(routes["legs"]["start_address"] + routes["legs"]["end_address"] + routes["legs"]["distance"]["text"])    
+        print(res["routes"][0]["legs"][0]["start_address"] + " to " + res["routes"][0]["legs"][0]["end_address"] +" Distance: " + res["routes"][0]["legs"][0]["distance"]["text"] + " Duration" +  res["routes"][0]["legs"][0]["duration"]["text"])
+        #return res
+        #print(res["routes"]["legs"]["start_address"])
+            #print(routes["legs"]["start_address"] + routes["legs"]["end_address"] + routes["legs"]["distance"]["text"])
+        data = {"start": res["routes"][0]["legs"][0]["start_address"], "end": res["routes"][0]["legs"][0]["end_address"],"length": res["routes"][0]["legs"][0]["distance"]["text"], "time": res["routes"][0]["legs"][0]["duration"]["text"]}
+        return json.dumps(data)
 
-findRoute("Bridgewater, Sa, Australia", "Stirling, SA, Australia")
+#findRoute("Bridgewater, Sa, Australia", "Stirling, SA, Australia")
