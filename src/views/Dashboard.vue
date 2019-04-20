c2d9ce768bb93ff3efbf3e09b7eb368518e0d20a
aaalana <41637549+aaalana@users.noreply.github.com>
2019-04-20 21:03:45 +1000

    fixing dashboard css

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="dasboard">
-    <v-container class="dblayout">
+    <v-content class="dblayout">
     <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
       <v-layout row>
         <v-flex xs12>
           <h1 style="font-family:Quicksand;padding:30px;font-size:30px">Map Mode</h1>
           <div id = dMap>
           <router-link to="/map">
-            <img id = "dMap" src="../assets/dMap.png" style="padding:30px" height="522px" width="1000px">
+          <img id = "dMap" src="../assets/dMap.png" style="padding:30px" height="522px" width="900px">
           </router-link>
           </div>
         </v-flex>
         <v-flex xs12>
-          <h2 style="font-family:Quicksand;padding:30px;font-size:30px">Nearby Attractions</h2>
+          <h2 style="font-family:Quicksand;padding-bottom:30px;padding-top:30px;font-size:30px">Nearby Attractions</h2>
           <p style="font-size:20px;padding:5px">Royal Botanical Gardens</p>
           <img id = "yeet" src="../assets/royalbg.png" style="padding:5px" height="232px" width="309px">
           <p style="font-size:20px;padding:5px">Anzac Memorial</p>
@@ -20,24 +20,20 @@
         </v-flex>
       </v-layout>
 
-      <v-layout row>
+      <v-layout row align-content-center mb-5>
         <v-flex xs12>
           <h3 style="font-family:Quicksand;padding:30px;font-size:30px">Road Trip!</h3>
-          <template>
-            <v-carousel>
+            <v-carousel style="width:90%">
               <v-carousel-item
                 v-for="(item,i) in items"
                 :key="i"
                 :src="item.src"
               ></v-carousel-item>
             </v-carousel>
-          </template>
         </v-flex>
       </v-layout>
-
-    </v-container>
-
-
+      <br><br>
+    </v-content>
     <ContactUs />
     <Menu />
     <Footer />
